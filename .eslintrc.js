c1f8624de20fae7465d33da09979a1ecc0d3cc62
Vipul Sahu <vipulsahu063@gmail.com>
2026-02-08 19:17:45 +0530

    fixed the eslint error

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,49 @@
-import globals from 'globals'
-import js from '@eslint/js'
-import stylisticJs from '@stylistic/eslint-plugin'
-
-export default [
-  js.configs.recommended,
-  {
-    files: ['**/*.js'],
-    languageOptions: {
-      sourceType: 'commonjs',
-      globals: { ...globals.node },
-      ecmaVersion: 'latest',
-    },
-    plugins: {
-      '@stylistic/js': stylisticJs,
-    },
-    rules: {
-      '@stylistic/js/indent': ['error', 2],
-      '@stylistic/js/linebreak-style': ['error', 'unix'],
-      '@stylistic/js/quotes': ['error', 'single'],
-      '@stylistic/js/semi': ['error', 'never'],
-      eqeqeq: 'error',
-      'no-trailing-spaces': 'error',
-      'object-curly-spacing': ['error', 'always'],
-      'arrow-spacing': ['error', { before: true, after: true }],
-      'no-console': 'off',
-    },
+module.exports = {
+  "env": {
+    "browser": true,
+    "es6": true,
+    "jest/globals": true
   },
-  {
-    ignores: ['dist/**'],
+  "extends": [
+    "eslint:recommended",
+    "plugin:react/recommended"
+  ],
+  "parserOptions": {
+    "ecmaFeatures": {
+      "jsx": true
+    },
+    "ecmaVersion": 2018,
+    "sourceType": "module"
   },
-]
+  "plugins": [
+    "react", "jest"
+  ],
+  "rules": {
+    "indent": [
+      "error",
+        2
+      ],
+    "linebreak-style": [
+      "error",
+      "\n"
+    ],
+    "quotes": [
+      "error",
+      "single"
+    ],
+    "semi": [
+      "error",
+      "never"
+    ],
+    "eqeqeq": "error",
+    "no-trailing-spaces": "error",
+    "object-curly-spacing": [
+      "error", "always"
+    ],
+    "arrow-spacing": [
+      "error", { "before": true, "after": true }
+    ],
+    "no-console": "error",
+    "react/prop-types": 0
+  }
+}
